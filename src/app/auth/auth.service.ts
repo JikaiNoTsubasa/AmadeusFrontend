98567ed53b785f7db5e6358f140d26093c681e99
Stéphane Biehler <biehler@s2i-evolution.eu>
2024-10-08 15:10:51 +0200

    ✨ Added login return
    Added login check and return object

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  httpClient = inject(HttpClient);
+  baseUrl = 'http://localhost:5008/auth';
+
   constructor() { }
+
+  login(data: any) {
+    return this.httpClient.post(`${this.baseUrl}/login`, data)
+      .pipe(tap((result) => {
+        //console.log(JSON.stringify(result));
+        sessionStorage.setItem('authUser', JSON.stringify(result));
+      }));
+  }
+
+  logout() {
+    sessionStorage.removeItem('authUser');
+  }
+
+  isLoggedIn() {
+    return sessionStorage.getItem('authUser') !== null;
+  }
 }
