854aa35696fbfe6736e1b637c28bbd48eedc725e
Stéphane Biehler <biehler@s2i-evolution.eu>
2024-10-17 16:02:05 +0200

    ✨ Fixed look
    Fixed layout and look

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { authGuard } from './auth/auth.guard';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { AppComponent } from './app.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 export const routes: Routes = [
     {
@@ -12,9 +14,12 @@ export const routes: Routes = [
         path: 'login', component: LoginComponent
     },
     {
+        path: 'unit/:id', component: ProjectComponent
+    },
+    {
         path: 'admin', component: AdminComponent, canActivate: [authGuard]
     },
     {
-        path: 'main', component: SidemenuComponent, canActivate: [authGuard]
+        path: 'main', component: AppComponent, canActivate: [authGuard]
     }
 ];
--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,19 +1,41 @@
 import { Component, inject, Input } from '@angular/core';
 import { Unit } from '../../Models/Unit';
 import { AmaService } from '../../services/AmaService';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
 
-  @Input() unit : Unit = null;
+  selectedUnit : Unit = null;
+  loading = true;
 
   amaService = inject(AmaService);
+  route = inject(ActivatedRoute);
+
+
+
+  ngOnInit(){
+    const id =this.route.snapshot.params['id'];
+    this.amaService.getUnit(id).subscribe({
+      next:(data) => {
+        this.selectedUnit = data;
+        }, 
+      error:(error)=>{
+        console.log(error);
+        },
+      complete:() => {
+        this.loading = false;
+        //console.log('complete');
+        }
+  });
+  }
 
   ngOnChanges(){
     console.log("On change unit");
