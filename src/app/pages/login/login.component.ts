69ef46815bce69e5d28d0a9a49e1879c9959acad
Stéphane Biehler <biehler@s2i-evolution.eu>
2024-11-05 11:31:57 +0100

    ✨ Added PrimeNG and started login rebuild
    Added PrimeNG and started login rebuild

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,11 +3,18 @@ import { inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ ReactiveFormsModule, RouterModule ],
+  imports: [ ReactiveFormsModule, RouterModule, CardModule, ButtonModule, InputTextModule, InputGroupModule, InputGroupAddonModule, PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -28,7 +35,6 @@ export class LoginComponent {
         if(this.authService.isLoggedIn()){
           this.router.navigate(['/main']);
         }
-        //console.log(data); 
       });
     }
   }
